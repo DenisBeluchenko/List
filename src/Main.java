d10876ad5d054a2aa90b24f8946aa842640adc14
Denis Beluchenko <dzibis@gmail.com>
2022-11-20 22:47:44 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,75 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] operation = {"Купить", "Показать", "Удалить","Найти"};
+        String[] operation = {"Купить", "Показать", "Удалить", "Найти"};
+        LinkedList<String> purchases = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < operation.length; i++) {
-            System.out.println(i + 1 + ". "  + operation[i]);
-        }
+
         int[] quantity = new int[3];
         int sumProducts = 0;
         while (true) {
-            int productNumber = 0;
-            int productCount = 0;
-            System.out.println("Выберите операцию:");
+            System.out.println("\nВыберите операцию:");
+            for (int i = 0; i < operation.length; i++) {
+                System.out.println(i + 1 + ". " + operation[i]);
+            }
             String inputString = scanner.nextLine();
-            if (inputString.equals("end")) {
-                break;
+            switch (inputString) {
+                case "1":
+                    System.out.println("Какую покупку хотите добавить?");
+                    inputString = scanner.nextLine();
+                    purchases.add(inputString);
+                    System.out.println("Итого в списке покупок:" + purchases.size());
+                    break;
+                case "2":
+                    out(purchases);
+                    break;
+                case "3":
+                    out(purchases);
+                    System.out.println("Какую хотите удалить? Введите номер или название");
+                    inputString = scanner.nextLine();
+                    if (isInt(inputString)) {
+                        int i = Integer.parseInt(inputString);
+                        System.out.println("Покупка " + i + " удалина,");
+                        purchases.remove(i - 1);
+                        out(purchases);
+                    } else {
+                        purchases.remove(inputString);
+                        System.out.println("Покупка " + inputString + " удалина,");
+                        out(purchases);
+                    }
+                    break;
+                case "4":
+                    System.out.println("Введите текст для поиска:");
+                    inputString = scanner.nextLine();
+                    System.out.println("Найдено:");
+                    for (int a = 0; a < purchases.size(); a++) {
+                        String itemLower=purchases.get(a).toLowerCase();
+                        String queryLower=inputString.toLowerCase();
+                        if (itemLower.contains(queryLower)) {
+                            System.out.println(+(a + 1) + ". " + purchases.get(a));
+                        }
+                    }
+                    break;
             }
-            String[] parts = inputString.split(" ");
-            if (parts.length != 2) {
-                System.out.println("Должно быть 2 числа через пробел или end");
-                continue;
-            }
-            try {
-                productNumber = Integer.parseInt(parts[0]);
-                productCount += Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                System.out.println("Должны быть числа, попробуйте ещё раз");
-            }
-            if (productNumber < 1 | productNumber > 3) {
-                System.out.println("В наличии товар от 1-3, попробуйте ещё раз");
-                continue;
-            }
-            if (productCount < 1) {
-                System.out.println("Количество тавара должго быть больше 0, попробуйте ещё раз");
-                continue;
-            }
-            quantity[productNumber - 1] += productCount;
         }
-/*        System.out.println("Ваша корзина:");
-        for (int i = 0; i < products.length; i++) {
-            if (quantity[i] == 0) {
-                continue;
-            }
-            System.out.println(products[i] + " " + quantity[i] + "шт " + prices[i] + " руб/шт " + prices[i] * quantity[i] + " руб в сумме");
-            sumProducts += prices[i] * quantity[i];
-        }*/
-        System.out.println("Итого " + sumProducts + " руб");
+    }
+
+    public static void out(LinkedList<String> purchases) {
+        System.out.println("Список покупок:");
+        for (int a = 0; a < purchases.size(); a++) {
+            System.out.println(+(a + 1) + ". " + purchases.get(a));
+        }
+    }
+
+    private static boolean isInt(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
 
